33a11e745d47fc12a3a3e577685e3e3961dc0b17
Zia-Saeed <zia.aseh@gmail.com>
2025-04-06 01:13:52 +0500

    deploy

---
+++ frontend/src/api/index.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: process.env.NODE_ENV === 'production' 
+      ? '/api/' 
+      : 'http://localhost:8000/api/',
+    withCredentials: true,
+  });
+
+export default api;
